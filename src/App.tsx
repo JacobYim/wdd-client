da0b0c940467a07d1a727208584cb27b85a1431d
JWWon <wonjiwn@naver.com>
2019-01-28 18:15:11 +0900

    Complete install aws amplify, Complete add auth & storage in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import Amplify from 'aws-amplify';
 
 import configStore from 'src/store';
+import awsconfig from 'src/aws-exports';
 import Router from 'src/pages/Router';
+
+Amplify.configure(awsconfig);
 
 const App: React.SFC<{}> = () => {
   const store = configStore();
