af8124279b40e05f83cdf6fc2723320c2b81e88c
JWWon <wonjiwn@naver.com>
2019-02-03 23:37:36 +0900

    Change develop server baseURL, Update README.md

--- README.md
+++ README.md
@@ -15,3 +15,8 @@
    > run-android는 에뮬레이터 / 실제 디바이스가 연결된 이후에 실행
 
    > run-ios는 macOS에서 XCode 설치 후 실행 가능
+
+### 참고
+
+1. iOS에서 DaumMap SDK를 정상적으로 불러오지 못하는 현상때문에, iOS용 SDK를 `node_modules/react-native-daummap/ios` 에 추가해주어야 한다. [관련 논의 내용](https://github.com/asata/react-native-daummap/issues/1)
+2. Android는 에뮬레이터에서 돌아가지 않는다. [관련 논의 내용](https://github.com/asata/react-native-daummap/issues/5)
--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Platform } from 'react-native';
 
 export function setHeader(token: string) {
   axios.defaults.headers.common['authorization'] = token;
@@ -8,8 +9,9 @@ export function removeHeader() {
   delete axios.defaults.headers.common['authorization'];
 }
 
+const BASE_URL = Platform.OS === 'ios' ? '127.0.0.1' : '10.0.2.2';
 export default function configAxios() {
   axios.defaults.baseURL = __DEV__
-    ? 'http://localhost:8080'
+    ? `http://${BASE_URL}:8080`
     : 'http://api.woodongdang.com';
 }
