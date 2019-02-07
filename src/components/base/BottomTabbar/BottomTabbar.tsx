5de2308d749349752857a599d0495728d0ee827f
JWWon <wonjiwn@naver.com>
2019-02-08 03:01:13 +0900

    Update main page bottom tab bar logic

--- src/components/base/BottomTabbar/BottomTabbar.tsx
+++ src/components/base/BottomTabbar/BottomTabbar.tsx
@@ -11,55 +11,40 @@ const BottomNavbar: React.FC<Props> = ({ navigation }) => {
     navigation.navigate(route.routeName);
   }
 
-  function renderTab(route: NavigationRoute, index: number) {
-    const isCurPage = index === navigation.state.index;
-    if (route.params) {
-      switch (index) {
-        case 0:
-          return isCurPage ? null : (
-            <TouchableOpacity
-              style={[views.centerTab, views.notWalking]}
-              key={route.routeName}
-              onPress={() => navigate(route)}
-              activeOpacity={0.94}>
-              <Image style={views.centerIcon} source={route.params.icon} />
-            </TouchableOpacity>
-          );
-        case 3:
-          return (
-            navigation.state.index === 0 && (
-              <TouchableOpacity
-                style={[views.centerTab, views.notWalking]}
-                key={route.routeName}
-                onPress={() => navigate(route)}
-                activeOpacity={0.94}>
-                <Image style={views.centerIcon} source={route.params.icon} />
-              </TouchableOpacity>
-            )
-          );
-        case 1:
-        case 2:
-          return (
-            <View style={views.tabWrapper} key={route.routeName}>
-              <TouchableOpacity
-                style={views.tabButton}
-                activeOpacity={0.7}
-                onPress={() => navigate(route)}
-                disabled={isCurPage}>
-                <Image style={views.tabIcon} source={route.params.icon} />
-                <Text style={texts.tabLabel}>{route.params.label}</Text>
-              </TouchableOpacity>
-            </View>
-          );
-      }
-    }
+  function renderTabs(route: NavigationRoute, index: number) {
+    if (!route.params) return null;
+    const navIndex = navigation.state.index;
+
+    if (index === 1 || index === 2)
+      return (
+        <View style={views.tabWrapper} key={route.routeName}>
+          <TouchableOpacity
+            style={views.tabButton}
+            activeOpacity={0.7}
+            onPress={() => navigate(route)}
+            disabled={index === navIndex}>
+            <Image style={views.tabIcon} source={route.params.icon} />
+            <Text style={texts.tabLabel}>{route.params.label}</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    if ((index === 0 && navIndex !== 0) || (index === 3 && navIndex === 0))
+      return (
+        <TouchableOpacity
+          style={[views.centerTab, views.notWalking]}
+          key={route.routeName}
+          onPress={() => navigate(route)}
+          activeOpacity={0.94}>
+          <Image style={views.centerIcon} source={route.params.icon} />
+        </TouchableOpacity>
+      );
   }
 
-  return (
+  return navigation.state.index !== 3 ? (
     <View style={views.container}>
-      {navigation.state.routes.map(renderTab)}
+      {navigation.state.routes.map(renderTabs)}
     </View>
-  );
+  ) : null;
 };
 
 export default BottomNavbar;
