6ba56a31abee2aa41d23a51f1d8fcf0c9e5cefb4
JWWon <wonjiwn@naver.com>
2019-02-14 01:27:59 +0900

    Apply styling on Walk page

--- src/pages/App/Walk/Walk.styles.ts
+++ src/pages/App/Walk/Walk.styles.ts
@@ -1,22 +1,130 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import { color, font } from 'src/theme';
+
+const { width } = Dimensions.get('window');
+const walkButton = width * 0.293;
+const peePooButton = 50;
+const whiteBackRadius =
+  (Math.pow(width / 2, 2) + Math.pow(walkButton / 2, 2)) / walkButton;
+const bottomPadding = width * 0.128;
 
 export const views = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: color.white,
   },
-  dashboard: {
-    flex: 1,
+  topWrapper: {
+    flex: 0.54,
+    alignItems: 'center',
+    zIndex: 1,
+  },
+  bottomWrapper: {
+    flex: 0.46,
+    backgroundColor: color.whiteF5,
+    alignItems: 'center',
+    zIndex: 0,
+  },
+  topButtonWrapper: {
+    width: '100%',
+    paddingHorizontal: 20,
+    marginTop: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  bottomButtonWrapper: {
+    width: '100%',
+    paddingHorizontal: bottomPadding,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  whiteBackground: {
+    position: 'absolute',
+    top: walkButton / 2 - 2 * whiteBackRadius,
+    left: width / 2 - whiteBackRadius,
+    width: whiteBackRadius * 2,
+    height: whiteBackRadius * 2,
+    borderRadius: whiteBackRadius,
+    backgroundColor: color.white,
+  },
+  walkButton: {
+    width: walkButton,
+    height: walkButton,
+    borderRadius: walkButton / 2,
+    backgroundColor: color.redLight,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  peePooButton: {
+    width: peePooButton,
+    height: peePooButton,
+    marginTop: width * 0.088 - peePooButton / 2,
+    borderRadius: peePooButton / 2,
+    backgroundColor: color.blue,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: color.black,
+    shadowOpacity: 0.16,
+    shadowRadius: 6,
+    shadowOffset: { width: 0, height: 3 },
+    elevation: 1,
+  },
+  gpsInfoWrapper: {
+    width: '100%',
+    marginTop: '4.5%',
+    paddingHorizontal: '11.5%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  gpsSingleInfo: {
+    width: width * 0.24,
+    height: 80,
+    borderRadius: 5,
+    backgroundColor: color.white,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: color.black,
+    shadowOpacity: 0.07,
+    shadowRadius: 6,
+    elevation: 1,
+  },
+});
+
+export const icons = StyleSheet.create({
+  top: { width: 24, height: 24, resizeMode: 'contain' },
+  topLeft: { alignSelf: 'flex-start' },
+  topRight: { alignSelf: 'flex-end' },
+  walkStatus: {
+    width: '43%',
+    height: '43%',
+    resizeMode: 'contain',
+  },
+  peePoo: {
+    width: '52%',
+    height: '52%',
+    resizeMode: 'contain',
   },
 });
 
 export const fonts = StyleSheet.create({
   walkTime: {
+    marginTop: 'auto',
+    marginBottom: 12,
     color: color.blue,
     fontSize: 68,
     fontFamily: font.family.NanumSquareRoundEB,
+    letterSpacing: -4,
+  },
+  gpsInfoValue: {
+    fontSize: 20,
+    fontFamily: font.family.NanumSquareRoundB,
+    letterSpacing: -0.8,
+    color: color.black,
+  },
+  gpsInfoUnit: {
+    marginTop: 6,
+    fontSize: font.size.medium,
+    fontFamily: font.family.NanumSquareRoundR,
+    letterSpacing: -0.5,
+    color: color.black,
   },
 });
--- src/pages/App/Walk/Walk.tsx
+++ src/pages/App/Walk/Walk.tsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
-import { withNavigation, NavigationScreenProp } from 'react-navigation';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import { NavigationScreenProp } from 'react-navigation';
 
 import Trailor from './Trailor';
-import { views, fonts } from './Walk.styles';
+import { views, fonts, icons } from './Walk.styles';
+
+interface GpsInfoInterface {
+  unit: string;
+  value: number;
+}
 
 interface Props {
   navigation: NavigationScreenProp<any>;
@@ -12,6 +23,9 @@ interface Props {
 interface State {
   shouldMountDashboard: boolean;
   walkTime: number;
+  distance: number;
+  step: number;
+  kcal: number;
 }
 
 function formatTime(time: number) {
@@ -22,9 +36,24 @@ function formatTime(time: number) {
 }
 
 class Walk extends Component<Props, State> {
+  private counter: NodeJS.Timeout & any;
+
   state: State = {
     shouldMountDashboard: false,
     walkTime: 0,
+    distance: 0,
+    step: 0,
+    kcal: 0,
+  };
+
+  startCountTime = () =>
+    setInterval(() => {
+      this.setState({ walkTime: this.state.walkTime + 1 });
+    }, 1000);
+
+  navToMap = () => {
+    const { navigation } = this.props;
+    navigation.navigate('map');
   };
 
   handleTrailorEnd = () => {
@@ -32,20 +61,74 @@ class Walk extends Component<Props, State> {
   };
 
   handleDashboardMount = () => {
-    setInterval(() => {
-      this.setState({ walkTime: this.state.walkTime + 1 });
-    }, 1000);
+    if (!this.counter) this.counter = this.startCountTime();
   };
 
   render() {
-    const { shouldMountDashboard, walkTime } = this.state;
+    const { shouldMountDashboard, walkTime, distance, step, kcal } = this.state;
+    const gpsInfoList: GpsInfoInterface[] = [
+      { value: distance, unit: 'Km' },
+      { value: step, unit: '걸음' },
+      { value: kcal, unit: 'Kcal' },
+    ];
 
     return (
       <SafeAreaView style={views.container}>
         {shouldMountDashboard ? (
-          <View style={views.dashboard} onLayout={this.handleDashboardMount}>
-            <Text style={fonts.walkTime}>{formatTime(walkTime)}</Text>
-          </View>
+          <>
+            <View style={views.topWrapper} onLayout={this.handleDashboardMount}>
+              <View style={views.topButtonWrapper}>
+                <TouchableOpacity activeOpacity={0.7} onPress={this.navToMap}>
+                  <Image
+                    style={[icons.top, icons.topLeft]}
+                    source={require('src/assets/icons/ic_map.png')}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity activeOpacity={0.7}>
+                  <Image
+                    style={[icons.top, icons.topRight]}
+                    source={require('src/assets/icons/ic_camera.png')}
+                  />
+                </TouchableOpacity>
+              </View>
+              <Text style={fonts.walkTime}>{formatTime(walkTime)}</Text>
+            </View>
+            <View style={views.bottomWrapper}>
+              <View style={views.whiteBackground} />
+              <View style={views.bottomButtonWrapper}>
+                <TouchableOpacity
+                  style={views.peePooButton}
+                  activeOpacity={0.7}>
+                  <Image
+                    source={require('src/assets/icons/ic_poo.png')}
+                    style={icons.peePoo}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity style={views.walkButton} activeOpacity={0.7}>
+                  <Image
+                    source={require('src/assets/icons/ic_pause.png')}
+                    style={icons.walkStatus}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={views.peePooButton}
+                  activeOpacity={0.7}>
+                  <Image
+                    source={require('src/assets/icons/ic_pee.png')}
+                    style={icons.peePoo}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={views.gpsInfoWrapper}>
+                {gpsInfoList.map(item => (
+                  <View style={views.gpsSingleInfo} key={item.unit}>
+                    <Text style={fonts.gpsInfoValue}>{item.value}</Text>
+                    <Text style={fonts.gpsInfoUnit}>{item.unit}</Text>
+                  </View>
+                ))}
+              </View>
+            </View>
+          </>
         ) : (
           <Trailor onFinish={this.handleTrailorEnd} />
         )}
@@ -54,4 +137,4 @@ class Walk extends Component<Props, State> {
   }
 }
 
-export default withNavigation(Walk);
+export default Walk;
