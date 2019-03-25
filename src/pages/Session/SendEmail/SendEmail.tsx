dbeb9823b45fac9a65369b49e4a707a90bc19629
JWWon <wonjiwn@naver.com>
2019-03-25 15:16:18 +0900

    Add image on SendEmail page

--- src/pages/Session/SendEmail/SendEmail.tsx
+++ src/pages/Session/SendEmail/SendEmail.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Dimensions, Image, View } from 'react-native';
 import { NavigationScreenProps } from 'react-navigation';
 import PageContainer from 'src/components/container/PageContainer';
+
+const { height } = Dimensions.get('window');
 
 const SendEmail: React.FC<NavigationScreenProps> = ({ navigation }) => (
   <PageContainer
     title="이메일을 전송했습니다!"
     subtitle="비밀번호 변경을 위해 이메일을 인증해주세요."
     right={{ view: '닫기', handlePress: () => navigation.popToTop() }}>
-    {}
+    <View style={{ marginTop: height * 0.15, alignItems: 'center' }}>
+      <Image
+        source={require('src/assets/images/img_mail.png')}
+        style={{ width: 189, height: 187 }}
+      />
+    </View>
   </PageContainer>
 );
 
