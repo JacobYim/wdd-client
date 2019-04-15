8d0a4fad607c2165de3b681d4e5f2082fbae6558
JWWon <wonjiwn@naver.com>
2019-04-16 04:44:20 +0900

    Add dismiss function on SendEmail page

--- src/pages/Session/SendEmail/SendEmail.tsx
+++ src/pages/Session/SendEmail/SendEmail.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import { Dimensions, Image, View } from 'react-native';
+import { Image, Keyboard, View } from 'react-native';
 import { NavigationScreenProps } from 'react-navigation';
 import PageContainer from 'src/components/container/PageContainer';
 
-const SendEmail: React.FC<NavigationScreenProps> = ({ navigation }) => (
-  <PageContainer
-    title="이메일을 전송했습니다!"
-    subtitle="비밀번호 변경을 위해 이메일을 인증해주세요."
-    right={{ view: '닫기', handlePress: () => navigation.popToTop() }}
-    alignTitleCenter>
-    <View style={{ marginTop: 80, alignItems: 'center' }}>
-      <Image
-        source={require('src/assets/images/img_mail.png')}
-        style={{ width: 189, height: 187 }}
-      />
-    </View>
-  </PageContainer>
-);
+const SendEmail: React.FC<NavigationScreenProps> = ({ navigation }) => {
+  Keyboard.dismiss();
+  return (
+    <PageContainer
+      title="이메일을 전송했습니다!"
+      subtitle="비밀번호 변경을 위해 이메일을 인증해주세요."
+      right={{ view: '닫기', handlePress: () => navigation.popToTop() }}
+      alignTitleCenter>
+      <View style={{ marginTop: 80, alignItems: 'center' }}>
+        <Image
+          source={require('src/assets/images/img_mail.png')}
+          style={{ width: 189, height: 187 }}
+        />
+      </View>
+    </PageContainer>
+  );
+};
 
 export default SendEmail;
