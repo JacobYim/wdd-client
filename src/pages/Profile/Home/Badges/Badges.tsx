8e60197c179bf50d600cc509ec82f077eef8fa0e
JWWon <wonjiwn@naver.com>
2019-04-16 06:31:16 +0900

    Fix banner space on android

--- src/pages/Profile/Home/Badges/Badges.tsx
+++ src/pages/Profile/Home/Badges/Badges.tsx
@@ -56,140 +56,139 @@ const Badges: React.FC<{ user: ReducerState['user'] }> = ({ user }) => {
     <>
       <Image
         source={require('src/assets/images/img_banner_wdd.png')}
-        style={{ width, resizeMode: 'contain' }}
+        style={{ width, height: width * 0.144, resizeMode: 'contain' }}
       />
-      <View style={{ flex: 1, paddingHorizontal: space / 2, marginTop: -8 }}>
-        <FlatList
-          data={[
-            {
-              label: '프로필 사진등록',
-              name: 'first_profile',
-              iconOn: require('src/assets/badges/badge_first_profile_on.png'),
-              iconOff: require('src/assets/badges/badge_first_profile_off.png'),
-              achieve: user.repDog ? user.repDog.thumbnail : false,
-            },
-            {
-              label: '첫 킁킁 보내기',
-              name: 'first_like',
-              iconOn: require('src/assets/badges/badge_first_like_on.png'),
-              iconOff: require('src/assets/badges/badge_first_like_off.png'),
-              achieve: user.repDog ? user.repDog.badges.firstLike : false,
-            },
-            {
-              label: '첫 산책 오줌',
-              name: 'first_pee',
-              iconOn: require('src/assets/badges/badge_first_pee_on.png'),
-              iconOff: require('src/assets/badges/badge_first_pee_off.png'),
-              achieve: firstPee,
-            },
-            {
-              label: '첫 산책 똥',
-              name: 'first_poo',
-              iconOn: require('src/assets/badges/badge_first_poo_on.png'),
-              iconOff: require('src/assets/badges/badge_first_poo_off.png'),
-              achieve: firstPoo,
-            },
-            {
-              label: '1km',
-              name: '1km',
-              iconOn: require('src/assets/badges/badge_1km_on.png'),
-              iconOff: require('src/assets/badges/badge_1km_off.png'),
-              achieve: distance >= 1,
-            },
-            {
-              label: '3km',
-              name: '3km',
-              iconOn: require('src/assets/badges/badge_3km_on.png'),
-              iconOff: require('src/assets/badges/badge_3km_off.png'),
-              achieve: distance >= 3,
-            },
-            {
-              label: '5km',
-              name: '5km',
-              iconOn: require('src/assets/badges/badge_5km_on.png'),
-              iconOff: require('src/assets/badges/badge_5km_off.png'),
-              achieve: distance >= 5,
-            },
-            {
-              label: '10km',
-              name: '10km',
-              iconOn: require('src/assets/badges/badge_10km_on.png'),
-              iconOff: require('src/assets/badges/badge_10km_off.png'),
-              achieve: distance >= 10,
-            },
-            {
-              label: '산책 1회',
-              name: 'walk_1',
-              iconOn: require('src/assets/badges/badge_walk_1_on.png'),
-              iconOff: require('src/assets/badges/badge_walk_1_off.png'),
-              achieve: feedLength > 0,
-            },
-            {
-              label: '산책 3회',
-              name: 'walk_3',
-              iconOn: require('src/assets/badges/badge_walk_3_on.png'),
-              iconOff: require('src/assets/badges/badge_walk_3_off.png'),
-              achieve: feedLength > 2,
-            },
-            {
-              label: '산책 5회',
-              name: 'walk_5',
-              iconOn: require('src/assets/badges/badge_walk_5_on.png'),
-              iconOff: require('src/assets/badges/badge_walk_5_off.png'),
-              achieve: feedLength > 4,
-            },
-            {
-              label: '산책 10회',
-              name: 'walk_10',
-              iconOn: require('src/assets/badges/badge_walk_10_on.png'),
-              iconOff: require('src/assets/badges/badge_walk_10_off.png'),
-              achieve: feedLength > 9,
-            },
-            {
-              label: '스크랩 1회',
-              name: 'scrap_1',
-              iconOn: require('src/assets/badges/badge_scrap_1_on.png'),
-              iconOff: require('src/assets/badges/badge_scrap_1_off.png'),
-              achieve: scrapLength > 0,
-            },
-            {
-              label: '스크랩 3회',
-              name: 'scrap_3',
-              iconOn: require('src/assets/badges/badge_scrap_3_on.png'),
-              iconOff: require('src/assets/badges/badge_scrap_3_off.png'),
-              achieve: scrapLength > 2,
-            },
-            {
-              label: '스크랩 5회',
-              name: 'scrap_5',
-              iconOn: require('src/assets/badges/badge_scrap_5_on.png'),
-              iconOff: require('src/assets/badges/badge_scrap_5_off.png'),
-              achieve: scrapLength > 4,
-            },
-            {
-              label: '스크랩 10회',
-              name: 'scrap_10',
-              iconOn: require('src/assets/badges/badge_scrap_10_on.png'),
-              iconOff: require('src/assets/badges/badge_scrap_10_off.png'),
-              achieve: scrapLength > 9,
-            },
-          ]}
-          showsVerticalScrollIndicator={false}
-          showsHorizontalScrollIndicator={false}
-          numColumns={4}
-          keyExtractor={(i, index) => index.toString()}
-          columnWrapperStyle={styles.container}
-          renderItem={({ item }) => (
-            <View style={styles.wrapper}>
-              <Image
-                source={item.achieve ? item.iconOn : item.iconOff}
-                style={styles.icon}
-              />
-              <Text style={styles.label}>{item.label}</Text>
-            </View>
-          )}
-        />
-      </View>
+      <FlatList
+        data={[
+          {
+            label: '프로필 사진등록',
+            name: 'first_profile',
+            iconOn: require('src/assets/badges/badge_first_profile_on.png'),
+            iconOff: require('src/assets/badges/badge_first_profile_off.png'),
+            achieve: user.repDog ? user.repDog.thumbnail : false,
+          },
+          {
+            label: '첫 킁킁 보내기',
+            name: 'first_like',
+            iconOn: require('src/assets/badges/badge_first_like_on.png'),
+            iconOff: require('src/assets/badges/badge_first_like_off.png'),
+            achieve: user.repDog ? user.repDog.badges.firstLike : false,
+          },
+          {
+            label: '첫 산책 오줌',
+            name: 'first_pee',
+            iconOn: require('src/assets/badges/badge_first_pee_on.png'),
+            iconOff: require('src/assets/badges/badge_first_pee_off.png'),
+            achieve: firstPee,
+          },
+          {
+            label: '첫 산책 똥',
+            name: 'first_poo',
+            iconOn: require('src/assets/badges/badge_first_poo_on.png'),
+            iconOff: require('src/assets/badges/badge_first_poo_off.png'),
+            achieve: firstPoo,
+          },
+          {
+            label: '1km',
+            name: '1km',
+            iconOn: require('src/assets/badges/badge_1km_on.png'),
+            iconOff: require('src/assets/badges/badge_1km_off.png'),
+            achieve: distance >= 1,
+          },
+          {
+            label: '3km',
+            name: '3km',
+            iconOn: require('src/assets/badges/badge_3km_on.png'),
+            iconOff: require('src/assets/badges/badge_3km_off.png'),
+            achieve: distance >= 3,
+          },
+          {
+            label: '5km',
+            name: '5km',
+            iconOn: require('src/assets/badges/badge_5km_on.png'),
+            iconOff: require('src/assets/badges/badge_5km_off.png'),
+            achieve: distance >= 5,
+          },
+          {
+            label: '10km',
+            name: '10km',
+            iconOn: require('src/assets/badges/badge_10km_on.png'),
+            iconOff: require('src/assets/badges/badge_10km_off.png'),
+            achieve: distance >= 10,
+          },
+          {
+            label: '산책 1회',
+            name: 'walk_1',
+            iconOn: require('src/assets/badges/badge_walk_1_on.png'),
+            iconOff: require('src/assets/badges/badge_walk_1_off.png'),
+            achieve: feedLength > 0,
+          },
+          {
+            label: '산책 3회',
+            name: 'walk_3',
+            iconOn: require('src/assets/badges/badge_walk_3_on.png'),
+            iconOff: require('src/assets/badges/badge_walk_3_off.png'),
+            achieve: feedLength > 2,
+          },
+          {
+            label: '산책 5회',
+            name: 'walk_5',
+            iconOn: require('src/assets/badges/badge_walk_5_on.png'),
+            iconOff: require('src/assets/badges/badge_walk_5_off.png'),
+            achieve: feedLength > 4,
+          },
+          {
+            label: '산책 10회',
+            name: 'walk_10',
+            iconOn: require('src/assets/badges/badge_walk_10_on.png'),
+            iconOff: require('src/assets/badges/badge_walk_10_off.png'),
+            achieve: feedLength > 9,
+          },
+          {
+            label: '스크랩 1회',
+            name: 'scrap_1',
+            iconOn: require('src/assets/badges/badge_scrap_1_on.png'),
+            iconOff: require('src/assets/badges/badge_scrap_1_off.png'),
+            achieve: scrapLength > 0,
+          },
+          {
+            label: '스크랩 3회',
+            name: 'scrap_3',
+            iconOn: require('src/assets/badges/badge_scrap_3_on.png'),
+            iconOff: require('src/assets/badges/badge_scrap_3_off.png'),
+            achieve: scrapLength > 2,
+          },
+          {
+            label: '스크랩 5회',
+            name: 'scrap_5',
+            iconOn: require('src/assets/badges/badge_scrap_5_on.png'),
+            iconOff: require('src/assets/badges/badge_scrap_5_off.png'),
+            achieve: scrapLength > 4,
+          },
+          {
+            label: '스크랩 10회',
+            name: 'scrap_10',
+            iconOn: require('src/assets/badges/badge_scrap_10_on.png'),
+            iconOff: require('src/assets/badges/badge_scrap_10_off.png'),
+            achieve: scrapLength > 9,
+          },
+        ]}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        numColumns={4}
+        keyExtractor={(i, index) => index.toString()}
+        style={{ flex: 1, paddingHorizontal: space / 2, marginTop: -8 }}
+        columnWrapperStyle={styles.container}
+        renderItem={({ item }) => (
+          <View style={styles.wrapper}>
+            <Image
+              source={item.achieve ? item.iconOn : item.iconOff}
+              style={styles.icon}
+            />
+            <Text style={styles.label}>{item.label}</Text>
+          </View>
+        )}
+      />
     </>
   );
 };
